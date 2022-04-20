fc595a057a449bc114b8d0d9d6d33f7c535b3826
matachew <76604256+matachew@users.noreply.github.com>
2022-04-20 23:33:53 +1200

    changed function name

--- Calculator.py
+++ Calculator.py
@@ -11,26 +11,26 @@ current = Entry(main, width=35, borderwidth=5)
 current.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 #e.insert(0, "")
-def add_to():
+def on_click():
     return
 
-button0 = Button(main, text="0", padx=100, pady=20, command=add_to)
-button1 = Button(main, text="1", padx=40, pady=20, command=add_to)
-button2 = Button(main, text="2", padx=40, pady=20, command=add_to)
-button3 = Button(main, text="3", padx=40, pady=20, command=add_to)
-button4 = Button(main, text="4", padx=40, pady=20, command=add_to)
-button5 = Button(main, text="5", padx=40, pady=20, command=add_to)
-button6 = Button(main, text="6", padx=40, pady=20, command=add_to)
-button7 = Button(main, text="7", padx=40, pady=20, command=add_to)
-button8 = Button(main, text="8", padx=40, pady=20, command=add_to)
-button9 = Button(main, text="9", padx=40, pady=20, command=add_to)
+button0 = Button(main, text="0", padx=100, pady=20, command=on_click)
+button1 = Button(main, text="1", padx=40, pady=20, command=on_click)
+button2 = Button(main, text="2", padx=40, pady=20, command=on_click)
+button3 = Button(main, text="3", padx=40, pady=20, command=on_click)
+button4 = Button(main, text="4", padx=40, pady=20, command=on_click)
+button5 = Button(main, text="5", padx=40, pady=20, command=on_click)
+button6 = Button(main, text="6", padx=40, pady=20, command=on_click)
+button7 = Button(main, text="7", padx=40, pady=20, command=on_click)
+button8 = Button(main, text="8", padx=40, pady=20, command=on_click)
+button9 = Button(main, text="9", padx=40, pady=20, command=on_click)
 
-equalbutton = Button(main, text="=", padx=40, pady=20, command=add_to)
-clearbutton = Button(main, text="C", padx=40, pady=20, command=add_to)
-plusbutton = Button(main, text="+", padx=40, pady=20, command=add_to)
-minusbutton = Button(main, text="-", padx=40, pady=20, command=add_to)
-multiplybutton = Button(main, text="x", padx=40, pady=20, command=add_to)
-dividebutton = Button(main, text="/", padx=40, pady=20, command=add_to)
+equalbutton = Button(main, text="=", padx=40, pady=20, command=on_click)
+clearbutton = Button(main, text="C", padx=40, pady=20, command=on_click)
+plusbutton = Button(main, text="+", padx=40, pady=20, command=on_click)
+minusbutton = Button(main, text="-", padx=40, pady=20, command=on_click)
+multiplybutton = Button(main, text="x", padx=40, pady=20, command=on_click)
+dividebutton = Button(main, text="/", padx=40, pady=20, command=on_click)
 
                                                           
 # making button widgets or objects or something for the GUI
