ef6983d91ad2e790e268e64b40f0383d8f77e0e6
matachew <76604256+matachew@users.noreply.github.com>
2022-04-20 20:54:34 +1200

    renamed root / added number buttons

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,23 @@
 from tkinter import *
 
-root = Tk()
-root.title("Calculator")
+main = Tk()
+main.title("Calculator")
 
-current = Entry(root, width=35 borderwidth=5)
+current = Entry(main, width=35, borderwidth=5)
 current.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+def add_to():
+    return
 
+button0 = Button(main, text="0", padx=40, pady=20, command=add_to)
+button1 = Button(main, text="1", padx=40, pady=20, command=add_to)
+button2 = Button(main, text="2", padx=40, pady=20, command=add_to)
+button3 = Button(main, text="3", padx=40, pady=20, command=add_to)
+button4 = Button(main, text="4", padx=40, pady=20, command=add_to)
+button5 = Button(main, text="5", padx=40, pady=20, command=add_to)
+button6 = Button(main, text="6", padx=40, pady=20, command=add_to)
+button7 = Button(main, text="7", padx=40, pady=20, command=add_to)
+button8 = Button(main, text="8", padx=40, pady=20, command=add_to)
+button9 = Button(main, text="9", padx=40, pady=20, command=add_to)
 
-root.mainloop()
+main.mainloop()
