83284cb890dc559d9871a7b424424c87a83ad3c9
matachew <76604256+matachew@users.noreply.github.com>
2022-04-20 14:45:08 +1200

    added current int. and a entryform?

--- Calculator.py
+++ Calculator.py
@@ -3,4 +3,9 @@ from tkinter import *
 root = Tk()
 root.title("Calculator")
 
+current = Entry(root, width=35 borderwidth=5)
+current.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+
+
 root.mainloop()
