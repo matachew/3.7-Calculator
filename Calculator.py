4580a2a5a03575468559a23a469b2e11fd989076
matachew <76604256+matachew@users.noreply.github.com>
2022-04-21 13:34:06 +1200

    resized buttons to look nicer

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from unicodedata import decimal
 
 
 
@@ -29,26 +30,30 @@ def on_equal():
     current.delete(0, END)
     current.insert(0, num1 + int(int2))
 
+def on_decimal():
+    return
 
-button0 = Button(main, text="0", padx=100, pady=20, command=lambda: on_click(0)) #footnote 1 at bottom about Lambda
-button1 = Button(main, text="1", padx=40, pady=20, command=lambda: on_click(1))
-button2 = Button(main, text="2", padx=40, pady=20, command=lambda: on_click(2))
-button3 = Button(main, text="3", padx=40, pady=20, command=lambda: on_click(3))
-button4 = Button(main, text="4", padx=40, pady=20, command=lambda: on_click(4))
-button5 = Button(main, text="5", padx=40, pady=20, command=lambda: on_click(5))
-button6 = Button(main, text="6", padx=40, pady=20, command=lambda: on_click(6))
-button7 = Button(main, text="7", padx=40, pady=20, command=lambda: on_click(7))
-button8 = Button(main, text="8", padx=40, pady=20, command=lambda: on_click(8))
-button9 = Button(main, text="9", padx=40, pady=20, command=lambda: on_click(9))
+
+
+button0 = Button(main, text="0", padx=65, pady=20, command=lambda: on_click(0)) #footnote 1 at bottom about Lambda
+button1 = Button(main, text="1", padx=20, pady=20, command=lambda: on_click(1))
+button2 = Button(main, text="2", padx=20, pady=20, command=lambda: on_click(2))
+button3 = Button(main, text="3", padx=20, pady=20, command=lambda: on_click(3))
+button4 = Button(main, text="4", padx=20, pady=20, command=lambda: on_click(4))
+button5 = Button(main, text="5", padx=20, pady=20, command=lambda: on_click(5))
+button6 = Button(main, text="6", padx=20, pady=20, command=lambda: on_click(6))
+button7 = Button(main, text="7", padx=20, pady=20, command=lambda: on_click(7))
+button8 = Button(main, text="8", padx=20, pady=20, command=lambda: on_click(8))
+button9 = Button(main, text="9", padx=20, pady=20, command=lambda: on_click(9))
 
 equalbutton = Button(main, text="=", padx=20, pady=20, command=on_equal)
 clearbutton = Button(main, text="C", padx=20, pady=20, command=on_clear)
-plusbutton = Button(main, text="+", padx=40, pady=20, command=on_add)
+plusbutton = Button(main, text="+", padx=20, pady=20, command=on_add)
 minusbutton = Button(main, text="-", padx=20, pady=20, command=lambda: on_click())
 multiplybutton = Button(main, text="x", padx=20, pady=20, command=lambda: on_click())
 dividebutton = Button(main, text="/", padx=20, pady=20, command=lambda: on_click())
 
-de
+decimalbutton = Button(main, text=".", padx=20, pady=20, command=on_decimal)
 
                                                           
 # making button widgets or objects or something for the GUI
